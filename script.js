b9c909508f995349c33556d2d6303280a76bb4fb
unfriedhayden <unfriedh6170@student.judsonisd.org>
2025-09-22 11:16:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const g = 9.8;
+let playerx = 0;
+let playery = 0;
+let px = 10;
+let py = 10;
 
+function newframe(){
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(px,px,playerx,playery);
+  ctx.font = '20px Arial';
+  ctx.fillStyle = 'white';
+  ctx.fillText('test',20,10);
+}
+while(true){
+  newframe();
+}
